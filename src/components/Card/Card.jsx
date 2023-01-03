0651c6ea94ddbf34f08c95fb54b48a3b92e60c64
Guilherme da Rocha <guis.darocha@gmail.com>
2023-01-03 20:25:25 -0300

    Criado seção Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CardContent } from './Card.style'
+
+
+const Card = () => {
+    return (
+      <CardContent>
+
+      </CardContent>
+    )
+  }
+
+export default Card
