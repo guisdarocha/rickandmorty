f5cdd768497547df47b204bf3e86f575f803a213
Guilherme da Rocha <guis.darocha@gmail.com>
2023-01-03 21:22:50 -0300

    Componente Botão feito

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/Header/Header'
 import Card from './components/Card/Card'
+import Botao from './components/Botao/Botao'
 
 
 function App() {
 
   return (
     <>
+
       <Header />
+      <div className="container">
       <Card />
+      <Card />
+      <Card />
+      <Card />
+      </div>
+      
+      <Botao />
 
     </>
   )
